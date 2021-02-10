ffe75fb3c6c065d6c451f4823a9b7429508e31b2
Alex Szabo <alex.szabo@prezi.com>
2021-02-10 13:04:32 +0100

    fix: Fix an issue where a callback was executing after the return statement
    feat: pass along filename, to babel, as some plugins require it

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const pluginBabel = (options = {}) => ({
 		const { filter = /.*/, namespace = '', config = {} } = options;
 
 		const transformContents = ({ args, contents }) => {
-			const babelOptions = babel.loadOptions(config);
+			const babelOptions = babel.loadOptions({ filename: args.path, ...config });
 
 			if (babelOptions.sourceMaps) {
 				const filename = path.relative(process.cwd(), args.path);
@@ -16,22 +16,26 @@ const pluginBabel = (options = {}) => ({
 				babelOptions.sourceFileName = filename;
 			}
 
-			babel.transform(contents, babelOptions, (error, result) => {
-				if (error) throw error;
-
-				contents = result.code;
+			return new Promise((resolve, reject) => {
+				babel.transform(contents, babelOptions, (error, result) => {
+					if (error) {
+						reject(error);
+					} else {
+						resolve({ contents: result.code });
+					}
+				});
 			});
-
-			return { contents };
 		};
 
-		if (transform) return transformContents(transform);
+		if (transform) {
+			return transformContents(transform);
+		} else {
+			build.onLoad({ filter, namespace }, async args => {
+				const contents = await fs.promises.readFile(args.path, 'utf8');
 
-		build.onLoad({ filter, namespace }, async args => {
-			const contents = await fs.promises.readFile(args.path, 'utf8');
-
-			return transformContents({ args, contents });
-		});
+				return transformContents({ args, contents });
+			});
+		}
 	}
 });
 
